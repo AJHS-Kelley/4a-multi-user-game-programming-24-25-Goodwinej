bc0e643b2fe49c98630cc84536bc5f354ea512dd
Goodwinej <142345252+Goodwinej@users.noreply.github.com>
2024-09-06 14:43:27 -0400

    finishing guess a number code.
     corrected the quotation marks....

--- .github/01_gaming_exercises/02_guess_a_number/guessNumber.cs
+++ .github/01_gaming_exercises/02_guess_a_number/guessNumber.cs
@@ -1,13 +1,43 @@
 // Guess a number, Goodwine jermya, 9/6/24 v0.00
-
 using System;
 class GuessNumber {
   static void Main() {
     int NumGuess = 0;
     int MaxGuess = 6;
+    int guess ;
 
   // Generates the secret number here.
+  Random rnd = new Random(); // create an object name "rnd" that is a copy of the Random()
+  int secretNumber = rnd.Next(100); // generate from 0 to 99
+  Console.WriteLine(secretNumber);
+  // int secretNumber = rnd.Next(23, 200); // generate from 25 to 199
+
   // while loop
+  while (NumGuess < MaxGuess)
+  {
+    Console.WriteLine ("secretNumber: " + secretNumber);
+    Console.WriteLine("Please guess an integer between 0 and 99.\n");
+    guess = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Guess"+ guess);
+    // Console.WriteLine(guess);
+    NumGuess++;
+    if (guess < secretNumber)
+    {
+      Console.WriteLine("your guess is too low.\n");
+    }
+    else if (guess > secretNumber)
+    {
+      Console.WriteLine("your guess is too high! \n");
+    }
+    else
+    {
+      Console.WriteLine("You guessed correctly. \n");
+      break;
+    }
+    if (NumGuess >= MaxGuess)
+    Console.WriteLine("you have lost the game. \n your grandparents are disappointed in you. \n")
+  }
+
      // Allow them to guess a number
      // Compare the guess to the secret number.
      // If match, print win, if > or <, give a hint.
